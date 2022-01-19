a3abdf43f0a8e81a246d14bf90de4548a2acf44b
Bevin Duncan <bevin.duncan@gmail.com>
2022-01-18 17:18:07 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(`${name}`)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(`${name}`)
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(`${name}`)
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(`${name}`)
+}
+function appendCat(name){
+    const appendCat = [...cats, "Broom"]
+    return appendCat
+}
+function prependCat(name){
+    const prependCat = ["Arnold", ...cats]
+    return prependCat
+}
+function removeLastCat(){
+    const removeLastCat = [cats]
+    return cats.slice(0, -1)
+}
+function removeFirstCat(){
+    const removeFirstCat = [cats]
+    return cats.slice(1)
+}
